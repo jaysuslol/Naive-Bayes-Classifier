0b1bf5e18a277a09857233c3fd9f4e7382482e65
Jason <fiasonask@gmail.com>
2024-01-28 19:29:22 +0200

    Added k-fold cross validation

--- Naive-Bayes.py
+++ Naive-Bayes.py
@@ -2,8 +2,8 @@ import os
 import numpy
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import BernoulliNB
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import cross_val_score
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
@@ -17,12 +17,23 @@ def run(user_options):
     documents = []
     classes = []
 
-    if (inp[1]): clf = MultinomialNB()
-    elif (inp[1] == 2): clf = BernoulliNB()
+    if (inp[1]):
+        clf = MultinomialNB()
+        vectorizer = CountVectorizer(binary=False)
+    elif (inp[1] == 2):
+        clf = BernoulliNB()
+        vectorizer = CountVectorizer(binary=True)
     else:
         print("Unrecognized option specified")
         exit(-1)
+
+    try:
+        if (inp[2] is not None):
+            K_FOLD_CROSS_VALIDATION = int(inp[2])
+    except IndexError:
+        K_FOLD_CROSS_VALIDATION = 5 ## K-fold cross validation default
     
+    print("\nCross validation: %d" %K_FOLD_CROSS_VALIDATION + "-fold\n\n")
     for root, dirs, files in os.walk(dataset):
         currClass = os.path.split(root)[1]
 
@@ -40,10 +51,20 @@ def run(user_options):
     classes = numpy.array(classes)
     #print(documents.shape)
     #print(classes.shape)
-    vectorizer = TfidfVectorizer()
     vectors = vectorizer.fit_transform(documents)
     clf.fit(vectors, classes)
+
+    score = cross_val_score(clf, vectors, classes, cv=K_FOLD_CROSS_VALIDATION)
+    if (inp[1]):
+        print("Model used: Multinomial Naive-Bayes (Non-Binary Term Occurences)\n")
+    elif (inp[1] == 2):
+        print("Model used: Bernoulli Naive-Bayes Binary Term Occurences)\n")
+
+    print("Results:")
+    print(score)
     
+    ### Uncomment below code to check files individually (input)
+    """
     while(True):
         fname = input("Training done. Enter filename to classify: ")
         datatext = ''
@@ -58,11 +79,7 @@ def run(user_options):
         vectors_test = vectorizer.transform([datatext])
         pred = clf.predict(vectors_test)
         print(pred)
-                
-    
-    #print(classes)
-    #print(sets)
-    #print(len(sets))
+    """     
 
 def main():
     # !!! DURING FIRST TIME USAGE, UNCOMMENT THE FOLLOWING LINES, RUN THE SCRIPT ONCE AND RECOMMENT THEM
@@ -71,7 +88,8 @@ def main():
     #exit(1)
 
     os.system('cls')
-    print("\nUsage: DATASET_FILENAME OPTION\n\nOptions:\n1: Multinomial (Term Occurences)\n2: Bernoulli (Binary Term Occurences)\n\n" 
+    print("\nUsage: DATASET_FILENAME OPTION K_FOLD_CROSS_VALIDATION (default: 5)\n\n"
+          + "OPTIONS:\n1: Multinomial (Term Occurences)\n2: Bernoulli (Binary Term Occurrences)\n\n" 
           + "Make sure that the dataset is in the same folder as the python script.\n")
     userinp = input("Input: ")
     run(userinp)
